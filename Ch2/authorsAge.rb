54516cdf59a13fa35af24e91467cc08193710194
jrdowns <jrdowns@containerstore.com>
2015-08-20 14:16:27 -0500

    Updated Ch2 challenges

---
+++ Ch2/authorsAge.rb
@@ -0,0 +1,4 @@
+# Author is 1,160 million seconds old. How old, in years, is he?
+
+age_years = (((((1160 * 1000000) / 60 ) /60 )/ 24) / 365)
+puts "The author is #{age_years} years old."
---
+++ Ch2/time.rb
@@ -0,0 +1,12 @@
+# How many hours in a year?
+# How many minutes in a decade?
+# How many seconds old am I?
+
+hours_year = 24 * 365
+puts "#{hours_year} hours in one year."
+
+minutes_decade = hours_year * 10 * 60
+puts "#{minutes_decade} minutes in a decade."
+
+age_seconds = hours_year * 27 * 60 * 60
+puts "I am #{age_seconds} old."
