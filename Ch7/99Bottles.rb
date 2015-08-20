7c20b955040508c909e1919b28a8846a4aa8ee05
CoderJr <jdowns06@gmail.com>
2015-08-19 20:53:14 -0500

    Initial file commit

--- Ch7/99Bottles.rb
+++ Ch7/99Bottles.rb
@@ -1,1 +1,12 @@
-#99 bottles of beers song
+bottles = 99
+while true
+  if bottles > 0
+    puts "#{bottles} bottles of beer on the wall. #{bottles} bottles of beer." 
+    puts "Take one down, pass it around, #{bottles - 1} bottles of beer on the wall."
+    bottles = bottles - 1
+  else
+    puts "No more bottles of beer on the wall, no more bottles of beer."
+    puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+    break
+  end
+end
