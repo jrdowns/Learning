1b04b999c9e8efd4600d669146ded111edfd13c3
jrdowns <jrdowns@containerstore.com>
2015-08-21 15:32:54 -0500

    Initial program commits

--- Ch8/arrays.rb
+++ Ch8/arrays.rb
@@ -2,3 +2,29 @@
 # End when you press Enter on an empty line.
 # Repeats words back in alphabetical order.
 # use .sort method
+puts "Type as many words as you'd like:"
+word_list = []
+
+while true
+  word = gets.chomp
+  if word == ""
+    break
+  end
+  
+  # Pushes input (word) into array (word_list).
+  word_list.push word
+end
+
+puts "Here are the words sorta alphabetically:"
+# .sort, by default, sorts alphabetically, but can sort in other ways.
+puts word_list.sort
+
+=begin
+Alternative method:
+while true
+  word = gets.chomp
+  break if word.empty?
+  
+  words << word
+end  
+=end
--- Ch8/tableofContents.rb
+++ Ch8/tableofContents.rb
@@ -5,3 +5,17 @@ of your table of contents (chapter names, page numbers, and so on).
 Then print out the information from the array in a beautifully formatted
 table of contents.
 =end
+line_width = 55
+contents = ["Table of Contents", "Chapter 1:  Getting Started", "page 1", "Chapter 2:  Numbers", "page 9", "Chapter 3:  Letters", "page 13"]
+
+puts contents[0].center(line_width)
+
+puts contents[1].ljust(line_width/2) + contents[2].rjust(line_width/2)
+puts contents[3].ljust(line_width/2) + contents[4].rjust(line_width/2)
+puts contents[5].ljust(line_width/2) + contents[6].rjust(line_width/2)
+
+=begin
+book uses the following:
+puts(contents[1].ljust(line_width/2))
+The the second set of ()? Seems to work without.  
+=end
