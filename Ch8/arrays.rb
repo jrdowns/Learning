2539e849e1f8e7570bda0ec6fc02074d9e409ac1
jrdowns <jrdowns@containerstore.com>
2015-08-21 14:43:50 -0500

    File updates

---
+++ Ch8/arrays.rb
@@ -0,0 +1,4 @@
+# Ask you to type as many words as you want. Stores in array.
+# End when you press Enter on an empty line.
+# Repeats words back in alphabetical order.
+# use .sort method
---
+++ Ch8/tableofContents.rb
@@ -0,0 +1,7 @@
+=begin
+Rewrite table of contents program from page 32.
+Start the program with an array holding all of the information
+of your table of contents (chapter names, page numbers, and so on).
+Then print out the information from the array in a beautifully formatted
+table of contents.
+=end
