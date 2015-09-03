ab41b03d69cf72627bae36c2e6bdb935f9920d17
jrdowns <jrdowns@containerstore.com>
2015-09-03 09:50:53 -0500

    added pluralization

--- Ch7/99Bottles.rb
+++ Ch7/99Bottles.rb
@@ -1,12 +1,15 @@
 bottles = 99
 while true
-  if bottles > 0
+  if bottles > 1
     puts "#{bottles} bottles of beer on the wall. #{bottles} bottles of beer." 
     puts "Take one down, pass it around, #{bottles - 1} bottles of beer on the wall."
     bottles = bottles - 1
   else
-    puts "No more bottles of beer on the wall, no more bottles of beer."
-    puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+    puts "#{bottles} bottle of beer on the wall. #{bottles} bottle of beer."
+    puts "Take one down, pass it around, #{bottles-1} bottles of beer on the wall"
     break
   end
 end
+
+puts "No more bottles of beer on the wall, no more bottles of beer."
+puts "Go to the store and buy some more, 99 bottles of beer on the wall."
--- Ch8/tableofContents.rb
+++ Ch8/tableofContents.rb
@@ -13,18 +13,17 @@ page_num = ["1", "9", "13"]
 line_start = "Chapter"
 page = "page"
 # Get line length to center title
-line_length = line_start.length + page.length + 35
-
-# Check to make sure chap_name, chap_num, and page_name aren't too long.
-
+# line_length = line_start.length + page.length + 35
+line_width = 55
 
 # OUTPUT
-puts title_name[0].center(line_length)
+puts title_name[0].center(line_width)
 
 puts " "
 
 chap_num.each_with_index do |chapter, x|
-  # Idea: encapsulate both halves and justify both halves line_width/2?
-  # (first half of variable).ljust(line_width/2) + (second half of variables).rjust(line_width/2)
-  puts line_start.ljust(line_start.length + 1) + chap_num[x].rjust(3) + ": " + chap_name[x].ljust(25) + page + " " + page_num[x].rjust(3)
+  first_half = line_start + " " + chap_num[x] + ": " + chap_name[x]
+  second_half = page + " " + page_num[x]
+
+  puts first_half.ljust(line_width/2) + second_half.rjust(line_width/2)
 end
