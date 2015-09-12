f839bc7c730decddc362175b4996ae4afb44af50
CoderJr <jdowns06@gmail.com>
2015-09-12 14:55:14 -0500

    added comment

--- Ch9/modernRoman.rb
+++ Ch9/modernRoman.rb
@@ -1,4 +1,33 @@
 =begin
 Rewrite previous method to return the new-style Roman numerals
 so when someone calls roman_numeral 4, it returns IV  
+I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000  
+
+INCOMPLETE. COPIED OVER OLDROMAN JUST AS A STARTING POINT
+
 =end
+
+def prompt
+  puts "Please enter an integer between 1 and 3000"
+  integer = gets.to_i
+  if integer < 1 || integer > 3000
+    puts "The number you entered does not fit within the range."
+    prompt
+  else  
+    return integer
+  end
+end
+
+def convert_to_roman num
+  m = "M" * (num / 1000)
+  d = "D" * (num % 1000 / 500)
+  c = "C" * (num % 500 / 100)
+  l = "L" * (num % 100 / 50)
+  x = "X" * (num % 50 / 10)
+  v = "V" * (num % 10 / 5)
+  i = "I" * (num % 5 / 1)
+  roman = m + d + c + l + x + v + i
+  # return roman
+end
+
+puts convert_to_roman(prompt)
