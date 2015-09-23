14b5b6db019b00ce4236bc33fc6311e83f8708af
jrdowns <jrdowns@containerstore.com>
2015-09-23 17:35:06 -0500

    initial commit

--- Ch10/sort.rb
+++ Ch10/sort.rb
@@ -2,3 +2,47 @@
 Write a program that sorts words alphabetically.  
 =end
 
+puts "Type as many words as you'd like. Enter a blank line to exit:"
+word_list = []
+
+while true
+  word = gets.chomp
+  if word == ""
+    break
+  end
+  
+  # Pushes input (word) into array (word_list).
+  word_list.push word
+end
+
+def sorting some_array
+  recursive_sort some_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+  #compare word A to all words in unsorted_array
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+  smallest = unsorted_array.pop
+  
+  still_unsorted = []
+  
+  unsorted_array.each do |x|
+    if x < smallest
+      still_unsorted.push smallest
+      smallest = x
+    else
+      still_unsorted.push x
+    end
+  end
+    sorted_array.push smallest
+    
+    recursive_sort still_unsorted, sorted_array
+end
+
+puts "Here is the list using .sort:"
+puts word_list.sort
+
+puts "here is the list using your program:"
+puts sorting word_list
---
+++ Ch10/weddingNumber.rb
@@ -0,0 +1,5 @@
+=begin
+It should work almost the same as english_number, except it should insert the
+word and all over the place, returning things like "nineteen hundred and
+seventy and two", or however wedding invitations are supposed to look.  
+=end
