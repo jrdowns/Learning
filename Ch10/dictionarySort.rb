cd9f5957124f812f034bac1bfc15d153d659b2c4
jrdowns <jrdowns@containerstore.com>
2015-09-23 17:44:55 -0500

    initial commit

--- Ch10/dictionarySort.rb
+++ Ch10/dictionarySort.rb
@@ -4,3 +4,47 @@ though, that if I give your program words starting with capital letters, it
 should return words with those came capital letters, just ordered as you'd
 find in a dictionary  
 =end
+
+puts "Type as many words as you'd like. Enter a blank line to exit:"
+word_list = []
+
+while true
+  word = gets.chomp
+  if word == ""
+    break
+  end
+  
+  word_list.push word
+end
+
+def sorting some_array
+  recursive_sort some_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+  
+  smallest = unsorted_array.pop
+  still_unsorted = []
+  
+  unsorted_array.each do |x|
+    if x.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = x
+    else
+      still_unsorted.push x
+    end
+  end
+  
+  sorted_array.push smallest
+    
+  recursive_sort still_unsorted, sorted_array
+end
+
+puts "Here is the list using .sort:"
+puts word_list.sort
+
+puts "Here is the list using your program:"
+puts sorting word_list
--- Ch10/shuffle.rb
+++ Ch10/shuffle.rb
@@ -4,3 +4,4 @@ version. As always, you'll want to test it, but testing this one is trickier:
 How can you test to make sure you are getting a perfect shuffle? What would
 you even say a perfect shuffle would be? Now test for it.  
 =end
+
