a76c51329e5e90b11b521caff1e77fa93809a3b5
jrdowns <jrdowns@containerstore.com>
2015-11-16 10:23:48 -0600

    initial commit

--- Ch10/expandedEnglish.rb
+++ Ch10/expandedEnglish.rb
@@ -40,16 +40,28 @@ def english_number number
                "fourteen", "fifteen", "sixteen",
                "seventeen", "eighteen", "nineteen"]
                
+  zillions = [["hundred", 2],
+              ["thousand", 3],
+              ["million", 6],
+              ["billion", 9],
+              ["trillion", 12]]
                
   left = number
-  write = left/100
-  left = left - write * 100
   
-  if write > 0
-    hundreds = english_number write
-    num_string = num_string + hundreds + " hundred"
-    if left > 0
-      num_string = num_string + " "
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write * zil_base
+    
+    if write > 0
+      prefix = english_number write
+      num_string = num_string + prefix + " " + zil_name
+      
+      if left > 0
+        num_string = num_string + " "
+      end
     end
   end
   
