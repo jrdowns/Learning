c9c9a2df5caff2f55c9addcdb947a38d25b05b3a
jrdowns <jrdowns@containerstore.com>
2016-01-18 12:51:40 -0600

    now fully functional

--- Ch13/orangeTree.rb
+++ Ch13/orangeTree.rb
@@ -1,16 +1,3 @@
-=begin
-  Orange trees start producing free after 3 years.
-  At 6 years, they produce 5-25 pounds of oranges.
-  They grow to a height of 25-50 feet.
-  At 10 years, they can produce 100 pounds of oranges.
-  Average orange weights 5-6 ounces.
-  16 ounces in 1 lb.
-  ~3 oranges in 1 lb.
-  ~300 oranges on 10 y/o tree
-  ~75 oranges on 6 y/o tree
-  Live to ~50 y/o
-=end
-
 class OrangeTree
 
   def initialize
@@ -18,16 +5,60 @@ class OrangeTree
     @age = 0 # A seedling
     @orange_count = 0
     puts "You plant your very own orange tree."
-    one_year_passes
+    command
+  end
+
+  def command
+    action_List = ["height", "age", "count oranges", "pick orange", "wait"]
+    puts "\n"
+    puts "What would you like to do?"
+    puts "Valid commands: height, age, count oranges, pick orange, or wait"
+    action = gets.downcase.chomp
+    puts "\n"
+    if action_List.include?(action)
+      if action == "height"
+        height
+      elsif action == "age"
+        years_old
+      elsif action == "count oranges"
+        count_the_oranges
+      elsif action == "pick orange"
+        pick_an_orange
+      elsif action == "wait"
+        wait
+      end
+    else
+      puts "OOPS! That is not a valid command!"
+      command
+    end
   end
 
   def height
     # Returns height of tree
-    puts "Your orange tree is #{@size} feet tall."
+    if @size < 1
+      puts "Your tree is still a seedling."
+    else
+      puts "Your orange tree is #{@size}' tall."
+    end
+    command
+  end
+
+  def years_old
+    if @age < 1
+      puts "Your tree is still a seedling."
+    else
+      puts "Your tree is #{@age} years old."
+    end
+    command
   end
 
   def count_the_oranges
-    puts "Your tree has #{@orange_count} oranges on it."
+    if @orange_count < 1
+      puts "Your tree has not produced any fruit yet."
+    else
+      puts "Your tree has #{@orange_count} oranges on it."
+    end
+    command
   end
 
   def pick_an_orange
@@ -35,23 +66,61 @@ class OrangeTree
       puts "You pick an orange from the tree and eat it. It was delicious."
       @orange_count -= 1
     else
-      puts "There are no more oranges on the tree."
+      puts "There are no oranges to pick."
     end
+    command
+  end
+
+  def wait
+    one_year_passes
+    command
   end
 
   private
 
+  def fruitful?
+    # Once tree reaches 3y/o, it will start bearing fruit.
+    @age >= 3
+  end
+
   def old?
     # Once tree reaches 20 y/o, it will start to whither, then die at 25.
+    # exit once dead
     @age >= 20
   end
 
   def one_year_passes
     puts "Your orange tree grows a foot taller over the course of the year."
     @size += 1
+    @age += 1
     # One year passes. Each year, tree grows taller.
     # Any oranges you don't pick fall off every year.
     # After some number of years, tree dies.
+    if old?
+      if @age < 25
+        puts "The tree begins to whither and bear less fruit as it grows old."
+      else
+        puts "Your tree has grown old and died."
+        exit # Quits the program
+      end
+    end
+
+    if fruitful?
+      if @age < 6
+        rand_count = rand(15..50)
+        @orange_count = rand_count
+      elsif @age >= 6 && @age < 10
+        rand_count = rand(50..100)
+        @orange_count = rand_count
+      elsif @age >= 10 && @age < 20
+        rand_count = rand(100..200)
+        @orange_count = rand_count
+      else
+        rand_count = rand(30..70)
+        @orange_count = rand_count
+      end
+      puts "Your tree grew #{@orange_count} oranges this year."
+    end
   end
 
 end
