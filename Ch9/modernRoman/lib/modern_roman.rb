081b01f0f47d0a5d14965d297d5f8997ebe110c7
James Downs <jrdowns@containerstore.com>
2017-04-07 15:28:33 +0000

    made some suggested rubocop changes

--- Ch9/modernRoman/lib/modern_roman.rb
+++ Ch9/modernRoman/lib/modern_roman.rb
@@ -1,6 +1,5 @@
 # I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
 class ModernRoman
-
   # Make sure input only contains numbers between 1 - 4999 & no letters
   def input_test(input)
     if input =~ /[a-zA-Z]+/ || input.to_i.between?(1, 4999) == false
@@ -9,60 +8,58 @@ class ModernRoman
       return true
     end
   end
-  
+
   def convert_to_roman(num)
     # M = 1000
-    m_num = (num/1000)
-    m = "M" * m_num
-  
+    m_num = (num / 1000)
+    m = 'M' * m_num
+
     # 100 - 900
     c_num = (num % 1000 / 100)
     if c_num == 9
-      c = "CM"
+      c = 'CM'
     elsif c_num > 5 && c_num < 9
-      c = "D" + "C" * (c_num - 5)
+      c = 'D' + 'C' * (c_num - 5)
     elsif c_num == 5
-      c = "D"
+      c = 'D'
     elsif c_num == 4
-      c = "CD"
+      c = 'CD'
     else
-      c = "C" * c_num
+      c = 'C' * c_num
     end
-  
+
     # 10 - 90
     x_num = (num % 100 / 10)
     if x_num == 9
-      x = "XC"
+      x = 'XC'
     elsif x_num > 5 && x_num < 9
-      x = "L" + "X" * (x_num - 5)
+      x = 'L' + 'X' * (x_num - 5)
     elsif x_num == 5
-      x = "L"
+      x = 'L'
     elsif x_num == 4
-      x = "XL"
+      x = 'XL'
     else
-      x = "X" * x_num
+      x = 'X' * x_num
     end
-  
+
     # 1 - 9
     i_num = (num % 10 / 1)
     if i_num == 9
-      i = "IX"
+      i = 'IX'
     elsif i_num > 5 && i_num < 9
-      i = "V" + "I" * (i_num - 5)
+      i = 'V' + 'I' * (i_num - 5)
     elsif i_num == 5
-      i = "V"
+      i = 'V'
     elsif i_num == 4
-      i = "IV"
+      i = 'IV'
     else
-      i = "I" * i_num
+      i = 'I' * i_num
     end
-  
+
     roman = m.to_s + c.to_s + x.to_s + i.to_s
-  
-    return roman
-    #puts roman
+
+    # puts roman
   end
-  
 end
 
-#ModernRoman.new
+# ModernRoman.new
---
+++ Ch9/modernRoman/test/test_modernroman.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'modernRoman'
+
+# TestModernRoman
+class TestModernRoman < MiniTest::Test
+  # Create test to make sure input_test works.
+  def test_input
+    input = ModernRoman.new
+    assert_equal(true, input.input_test('1'), 'This should pass true;
+                 the input (1) is within bounds.')
+    assert_equal(true, input.input_test('1117'), 'This should pass true;
+                 the input (1117) is within bounds.')
+    assert_equal(true, input.input_test('4999'), 'This should pass true;
+                 the input (4999) is within bounds')
+    assert_equal(false, input.input_test('0'), 'This should pass false;
+                 0 is out of bounds.')
+    assert_equal(false, input.input_test('5000'), 'This should pass false;
+                 5000 is out of bounds.')
+    assert_equal(false, input.input_test('1a44'), 'This should pass false;
+                 1a44 contains a letter.')
+  end
+
+  # Test number to Roman Numeral conversion
+  def test_number
+    number = ModernRoman.new
+    assert_equal('IV', number.convert_to_roman(4),
+                 'The Roman Numeral should be IV.')
+    assert_equal('XIV', number.convert_to_roman(14),
+                 'The Roman Numeral should be XIV.')
+    assert_equal('CDXI', number.convert_to_roman(411),
+                 'The Roman Numeral should be CDXI.')
+    assert_equal('MCXVII', number.convert_to_roman(1117),
+                 'The Roman Numeral should be MCXVII.')
+    assert_equal('MMMMCMXCIX', number.convert_to_roman(4999),
+                 'The Roman Numeral should be MMMMCMXCIX.')
+  end
+end
